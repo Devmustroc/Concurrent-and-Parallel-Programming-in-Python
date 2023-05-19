b36c8aacc8572a06f39a7978cf83e93f618a5cf5
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-19 11:34:33 +0200

    Asycn Python

---
+++ Python Async IO/main.py
@@ -0,0 +1,29 @@
+import time
+
+
+def calculate_sum_square(n):
+    sum_square = 0
+    for i in range(n):
+        sum_square += i ** 2
+    print(sum_square)
+
+
+def sleep_a_title(seconds):
+    time.sleep(seconds)
+
+
+def main():
+    calc_start_time = time.time()
+    for i in range(5):
+        calculate_sum_square((i + 1) * 1000000)
+    print('calculating sum of squares took: ', round(time.time() - calc_start_time, 1))
+
+    sleep_start_time = time.time()
+
+    for i in range(1, 6):
+        sleep_a_title(i)
+
+    print('sleeping took: ', round(time.time() - sleep_start_time, 1))
+
+if __name__ == '__main__':
+    main()
